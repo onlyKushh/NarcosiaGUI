24f93578213dbb26da39d57cbbd1d91a06720d0a
Kushh <65860709+onlyKushh@users.noreply.github.com>
2022-01-14 00:04:24 +0100

    Update NarcosiaGUI.lua

--- NarcosiaGUI.lua
+++ NarcosiaGUI.lua
@@ -773,7 +773,9 @@ LogsClose.Position = UDim2.new(-0.217434779, 0, 1.07639468, 0)
 LogsClose.Size = UDim2.new(0, 25, 0, 25)
 LogsClose.Image = "rbxassetid://3570695787"
 LogsClose.ImageColor3 = Color3.new(0.117647, 0.117647, 0.117647)
-LogsClose.SliceScale = 0.050
+NarcoLogin.ScaleType = Enum.ScaleType.Slice
+NarcoLogin.SliceCenter = Rect.new(100, 100, 100, 100)
+NarcoLogin.SliceScale = 0.050
 
 LogsCloseBtn.Name = "LogsCloseBtn"
 LogsCloseBtn.Parent = LogsClose
